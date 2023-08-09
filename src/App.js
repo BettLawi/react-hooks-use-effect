0302057a91f790f3c0e1f736206c3a205f6be851
lawi <lawi.bett@student.moringaschool.com>
2023-08-09 12:52:44 +0300

    new attempt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
+
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [today, setToday]=useState(new Date())
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+  // use effect to update the title of the dom
+  document.title=text
+  // telling react to set a time out 
 
-  console.log("Component rendering");
+  setTimeout(()=>setCount(0),1000)
+  setInterval(()=>{setToday(new Date())},1000)  
 
-  return <button>Click Me</button>;
+
+    // to stop the use effect from running each time we rerender we add an argument called array dependency
+  },[text,count]);
+
+  console.log("Component rendering ");
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={(e) => handleClick()}>
+        I've being clicked {count} times
+      </button>
+      <br></br>
+      <input
+        type="text"
+        placeholder="type away ..."
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+
+
+      <div>
+        {today.toString()}
+      </div>
+    </div>
+  );
 }
 
 export default App;
